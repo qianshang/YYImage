9f350ac686b47a02e8ef5c6c8239677d52f36d9b
chengwei <c1027999177@gmail.com>
2021-12-17 16:58:54 +0800

    ios14 不正常显示图片

--- YYImage/YYAnimatedImageView.m
+++ YYImage/YYAnimatedImageView.m
@@ -531,6 +531,10 @@ typedef NS_ENUM(NSUInteger, YYAnimatedImageType) {
 - (void)displayLayer:(CALayer *)layer {
     if (_curFrame) {
         layer.contents = (__bridge id)_curFrame.CGImage;
+    } else {
+        if (@available(iOS 14.0, *)) {
+            [super displayLayer:layer];
+        }
     }
 }
 
